fdcb215652f2fafb0b36515dba98e1659195ab25
tacolin <potegrant@yahoo.com.tw>
2015-02-24 19:57:20 +0800

    add -fPIC

--- 03_library/compile.sh
+++ 03_library/compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gcc -Werror -shared -g -pthread dtls.c -o libdtls.so -lcrypto -lssl
+gcc -fPIC -Werror -shared -g -pthread dtls.c -o libdtls.so -lcrypto -lssl
 
 gcc -L./ -Werror -Wl,-rpath=./ -g main.c -o main -ldtls
